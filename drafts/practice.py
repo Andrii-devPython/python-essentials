08f70b649a510232f0cbbebec9cb5afe90bfbab5
Andrii-devPython <aruabush@gmail.com>
2026-02-24 21:03:23 +0100

    Created practice.py in drafts, and gone throught variables, typecasting, input() and made first exercise

---
+++ drafts/practice.py
@@ -0,0 +1,71 @@
+#COMMENT ALL - CMD K - CMD D -- UNC CMD K - CMD U
+#2² control + command + space
+
+# Variable = a container for a value (string, float, boolean, integer)
+# A variable behaves as if it was the value it contains
+
+# region
+# Strings
+
+messageOnTg = "Hey mom, thanks for that"
+heardAfterThanks = "You are welcome"
+
+# Integers
+
+sumOfGoals = 5
+yourAge = 21
+
+# Floats
+
+priceOfToken = 10.99
+gpa = 3.2
+
+# Booleans
+
+isStudent = True
+forSale = False
+# endregion
+
+
+
+# Typecasting = the process of converting a variable from one data type to another
+# str(), int(), float(), bool() 
+
+# region
+
+#name = "Bro Code"
+#age = 25
+#gpa = 3.2
+#isStudent = True
+
+#gpa = int(gpa)
+
+# print(gpa)
+
+# endregion
+
+
+
+# input() = A function that prompts the user to enter data
+#           Returns the entered data as a string
+
+# region
+
+# myName = input("What is your name?: ")
+# myAge = input("How old are you?: ")
+
+# myAge = myAge + 1
+
+# print(f"Hello {myName}!")
+# print("HAPPY BIRTHDAY")
+# print(f"I am {myAge} years old!!!")
+
+# endregion
+
+# Exercise 1 Rectangle Area Calc
+
+length = float(input("Input length: "))
+width = float(input("Input width: "))
+areaCalc = length * width
+print(f"Rectangle Area Calc is: {areaCalc}cm²")
+
