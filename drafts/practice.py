90b6dbaf78e5565135b37e833ed3d6468fa4e9a0
Andrii-devPython <aruabush@gmail.com>
2026-02-26 23:26:16 +0100

    Started Madlibs game application

--- drafts/practice.py
+++ drafts/practice.py
@@ -71,10 +71,20 @@
 
 # Exercise 2 Shopping Cart Program
 
-item = input("What item would you like to buy?: ")
-price = float(input("What is the price?: "))
-quantity = int(input("How many would you like?: "))
-total = price * quantity
+#item = input("What item would you like to buy?: ")
+#price = float(input("What is the price?: "))
+#quantity = int(input("How many would you like?: "))
+#total = price * quantity
 
-print(f"You have bought {quantity} x {item}'s")
-print(f"Your total is: ${total}")
+#print(f"You have bought {quantity} x {item}'s")
+#print(f"Your total is: ${total}")
+
+# Madlibs game
+
+adjective1 = input("Enter an adjective (description): ")
+noun1 = input()
+
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
