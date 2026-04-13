ed8199b134c1cb9174a070fa3fe7eff72f340d8d
Andrii-devPython <aruabush@gmail.com>
2026-04-13 13:58:15 +0200

    Add info about conditional expressions

--- drafts/practice.py
+++ drafts/practice.py
@@ -117,3 +117,8 @@
 #               and = both conditions must be true
 #               not = inverts the condition (not False, not True)
 
+
+# conditional expression = A one-line shortcut for the if-else statement (ternary operator)
+#                          Print or assign one of two values based on a condition
+#                          X if condition else Y
+
